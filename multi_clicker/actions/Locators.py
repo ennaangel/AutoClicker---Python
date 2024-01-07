90a5c9322a2585a1aec338aaf8dfad224a79703b
ennaangel <chrisvos1992@gmail.com>
2024-01-07 18:18:25 +0100

    Add docstring to ImageFinder

--- multi_clicker/actions/Locators.py
+++ multi_clicker/actions/Locators.py
@@ -7,6 +7,14 @@ class iLocator:
 
 class ImageFinder(iLocator):
     def __init__(self, image_file: str, folder: str = "", confidence = 0.9, grayscale = False) -> None:
+            """Find centre of image
+
+        Args:
+            image_file (str): file name of image
+            folder (str, optional): Folder path of image. Defaults to "".
+            confidence (float, optional): Confidence level of found image, 1 is 100% sure. Defaults to 0.9.
+            grayscale (bool, optional): Enable Grayscale. Defaults to False.
+        """
         self.path = os.path.join(folder, image_file)
         self.confidence = confidence 
         self.grayscale = grayscale
