aeddb8b8c2b8f9d5df630cf5d792e5999e34de86
ennaangel <chrisvos1992@gmail.com>
2024-01-07 23:38:48 +0100

    Grabbing the right min_duraiotn parameter from the dict

--- multi_clicker/actions/Factories.py
+++ multi_clicker/actions/Factories.py
@@ -32,7 +32,7 @@ class ClickActionFactory(iActionFactory):
 class SleepActionFactory(iActionFactory):
     def create_action(self, parameters: dict) -> Actions.SleepAction:
         Sleeper = Sleepers.create_sleeper(parameters['parameters'])
-        min_duration_secs = parameters.get('duration', 0) # Set min duration to 0 if not givrn
+        min_duration_secs = parameters.get('min_duration_secs', 0) # Set min duration to 0 if not givrn
         return Actions.SleepAction(Sleeper = Sleeper, duration_secs = parameters['duration'], min_duration_secs = min_duration_secs)
     
 class LocatorClickerActionFactory(iActionFactory):
