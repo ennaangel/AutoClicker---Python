3d5191c56c2ef0c96da8ba4ef4479376133a25a1
ennaangel <chrisvos1992@gmail.com>
2024-01-07 21:22:59 +0100

    Add displace option to ImageFinder

--- Tests/test_ActionLocators.py
+++ Tests/test_ActionLocators.py
@@ -9,8 +9,15 @@ def test_image_finder():
     print(result)
     assert result == (29, 1056), "Wrong windows logo location"
 
+def test_image_displace():
+    ImFi = Locators.ImageFinder(image_file = 'testWindowLogo.png', folder = './Tests/TestData/', displace_pixels = (5,5))
+    result = ImFi.locate()
+    print(result)
+    assert result == [34, 1061], "Window location wronly displaced"
+
 def main():
     test_image_finder()
+    test_image_displace()
 
 if __name__ == "__main__":
     main()
--- multi_clicker/actions/Locators.py
+++ multi_clicker/actions/Locators.py
@@ -6,7 +6,7 @@ class iLocator:
         pass
 
 class ImageFinder(iLocator):
-    def __init__(self, image_file: str, folder: str = "", confidence = 0.9, grayscale = False) -> None:
+    def __init__(self, image_file: str, folder: str = "", confidence = 0.9, grayscale = False, displace_pixels: tuple = None) -> None:
         """Find centre of image
 
         Args:
@@ -18,6 +18,7 @@ class ImageFinder(iLocator):
         self.path = os.path.join(folder, image_file)
         self.confidence = confidence 
         self.grayscale = grayscale
+        self.displace_pixels = displace_pixels
 
     def locate(self):
         """Returns centra location of the image, if not found return None"""
@@ -25,4 +26,20 @@ class ImageFinder(iLocator):
             location = pyautogui.locateOnScreen(self.path, confidence = self.confidence, grayscale = self.grayscale)
         except pyautogui.ImageNotFoundException:
             return None
-        return pyautogui.center(location) 
+        location = pyautogui.center(location)
+        if self.displace_pixels != None:
+            location = self.displace(location = location, displace_pixels = self.displace_pixels)
+        return location
+    
+    def displace(self, location: tuple, displace_pixels: tuple)-> list:
+        """Displaces given location with supplied displace tuple, returning the new location as a list
+        Args:
+            location (tuple): initial location as tuple or box type
+            displace_pixels (tuple): displace
+        Returns:
+            list: _description_
+        """
+        location_list: list = [0,0]
+        location_list[1] = location[1] + displace_pixels[1]
+        location_list[0] = location[0] + displace_pixels[0]
+        return location_list
