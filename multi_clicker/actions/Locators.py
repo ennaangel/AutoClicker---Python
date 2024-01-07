dbd5bd90a494b17ea7ed60a3137a7e41f67d3856
ennaangel <chrisvos1992@gmail.com>
2024-01-07 17:56:58 +0100

    Added ImageFinder locator

--- multi_clicker/actions/Locators.py
+++ multi_clicker/actions/Locators.py
@@ -1,17 +1,20 @@
 import pyautogui
+import os
 
 class iLocator:
     def locate(self):
         pass
 
 class ImageFinder(iLocator):
-    def __init__(self, image_file: str, folder: str = "") -> None:
-        self.path
+    def __init__(self, image_file: str, folder: str = "", confidence = 0.9, grayscale = False) -> None:
+        self.path = os.path.join(folder, image_file)
+        self.confidence = confidence 
+        self.grayscale = grayscale
 
     def locate(self):
         """Returns centra location of the image, if not found return None"""
         try:
-            location = pyautogui.locateOnScreen(path, confidence = confidence, grayscale = grayscale)
+            location = pyautogui.locateOnScreen(self.path, confidence = self.confidence, grayscale = self.grayscale)
         except pyautogui.ImageNotFoundException:
             return None
         return pyautogui.center(location) 
