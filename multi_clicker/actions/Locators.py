8805550d0d7e10c2d55a016adcbf1279e9b70e1e
ennaangel <chrisvos1992@gmail.com>
2024-01-07 17:18:20 +0100

    Create Locators

---
+++ multi_clicker/actions/Locators.py
@@ -0,0 +1,10 @@
+class iLocator:
+    def locate(self):
+        pass
+
+class ImageFinder(iLocator):
+    def __init__(self, image_file: str, folder: str = "") -> None:
+        self.path
+
+    def locate(self):
+        return 
