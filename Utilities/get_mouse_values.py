79233f7df41c535ee0d47d8b66a97a56fac4caf6
ennaangel <chrisvos1992@gmail.com>
2024-01-11 23:09:30 +0100

    Simplified Utilities by removing dead code and moving code into one file

--- Utilities/get_mouse_values.py
+++ Utilities/get_mouse_values.py
@@ -1,11 +1,24 @@
 
 import time
-import ClickerFunctions as ClickerFunctions
+import pyautogui
 
+def fail_safe(x = 0, y = 0):
+    """Abort program if mouse position is (x,y)"""
+    pos: tuple = pyautogui.position()
+    if pos == (x,y):
+        message: str = f'Cancelled becaus of mouse position ({x},{y})'
+        raise Exception(message)
+    
+def get_mouse_pos_info()-> dict:
+    pos: tuple = pyautogui.position()
+    print(f'- position:{pos}')
+    hex_colour = pyautogui.pixel(pos[0], pos[1])
+    print(f'- colour: {hex_colour}')
+    return {'position': pos, 'hex_colour':hex_colour}
 
 
 for i in range(50):
     time.sleep(0.5)
-    ClickerFunctions.fail_safe(x = 0, y = 0)
-    ClickerFunctions.get_mouse_pos_info()
+    fail_safe(x = 0, y = 0)
+    get_mouse_pos_info()
     
